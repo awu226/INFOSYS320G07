2f4625a521551c942272e971a88d7264d4080852
awu226 <awu226@aucklanduni.ac.nz>
2016-08-21 11:51:10 +1200

    Linked Title screen to Main

---
+++ AucklandMuseum/Assets/NextScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour 
+{
+	public void NextLevelButton(int index)
+	{
+		Application.LoadLevel(index);
+	}
+
+	public void NextLevelButton()
+	{
+		Application.LoadLevel("Main");
+	}
+}
