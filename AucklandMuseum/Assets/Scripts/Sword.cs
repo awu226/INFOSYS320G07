df29c997d3bcfbc624b66393032a04bb3e013938
Chahat Chawla <ccha504@aucklanduni.ac.nz>
2016-09-22 15:35:13 +1200

    Added sword gallery - press c to change scenes

---
+++ AucklandMuseum/Assets/Scripts/Sword.cs
@@ -0,0 +1,27 @@
+﻿	using UnityEngine;
+	using System.Collections;
+
+	public class Sword : MonoBehaviour {
+	
+		
+		Animator anim;
+		// Use this for initialization
+
+		
+
+		void OnTriggerEnter(Collider coll){
+		Debug.Log ("Trigger");
+			if(coll.gameObject.name.Equals("Player"))
+			{
+			Debug.Log ("Player");
+			if(Input.GetKey(KeyCode.C))
+				{
+				Debug.Log ("c");
+					Application.LoadLevel("Title");
+
+				}
+			}
+		}
+		
+
+	}
