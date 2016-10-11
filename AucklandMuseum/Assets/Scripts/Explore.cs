1162dcd78ae19af6b7f106dd88c8d7b7bead644f
Chahat Chawla <ccha504@aucklanduni.ac.nz>
2016-10-11 14:51:34 +1300

    linked people to weapons

---
+++ AucklandMuseum/Assets/Scripts/Explore.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Explore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickDagger(){
+
+		SceneManager.LoadScene ("Dagger");
+	}
+	public void OnClickSword(){
+
+		SceneManager.LoadScene ("Sword1");
+	}
+	public void OnClickCutter(){
+
+		SceneManager.LoadScene ("Cutter");
+	}
+
+	public void OnClickKatana(){
+
+		SceneManager.LoadScene ("Katana");
+	}
+	public void OnClickRapier(){
+
+		SceneManager.LoadScene ("Rapier");
+	}
+	public void OnClickEpic(){
+
+		SceneManager.LoadScene ("SwordEpic");
+	}
+
+
+}
---
+++ AucklandMuseum/Assets/Scripts/LinkingScript.cs
@@ -0,0 +1,35 @@
+﻿	using UnityEngine;
+	using System.Collections;
+	using UnityEngine.SceneManagement;
+
+	public class LinkingScript : MonoBehaviour {
+	
+		
+		Animator anim;
+		// Use this for initialization
+
+		
+
+		void OnTriggerEnter(Collider coll){
+		Debug.Log ("Trigger");
+		if (coll.gameObject.name.Equals ("Player")) {
+
+			if (transform.name.Equals ("CDagger")) {
+				Debug.Log (transform.name);
+				SceneManager.LoadScene ("Dagger");
+			} else if (transform.name.Equals ("CSword")) {
+				SceneManager.LoadScene ("Sword1");
+			} else if (transform.name.Equals ("CRapier")) {
+				SceneManager.LoadScene ("Rapier");
+			} else if (transform.name.Equals ("CKatana")) {
+				SceneManager.LoadScene ("Katana");
+			} else if (transform.name.Equals ("CCutter")) {
+				SceneManager.LoadScene ("Cutter");
+			} else if (transform.name.Equals ("CEpic")) {
+				SceneManager.LoadScene ("SwordEpic");
+			}
+		}
+		
+
+	}
+	}
